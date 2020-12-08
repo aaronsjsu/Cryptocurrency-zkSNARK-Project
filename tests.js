440520bd7e9c1aaa31cad6089d8fb2cd5f06ef81
Aaron Smith <aaron.smith@sjsu.edu>
2020-12-07 19:52:10 -0800

    added deserialization methods, and tests for them

--- tests.js
+++ tests.js
@@ -2,9 +2,14 @@ const snarkjs = require("snarkjs");
 const fs = require("fs");
 const crypto = require("crypto");
 const zksnarkUtils = require("./zk-snark-utils.js");
+const ZKSnarkBlockchain = require('./zk-snark-blockchain.js');
+const ZKSnarkBlock = require('./zk-snark-block.js');
+const ZKSnarkClient = require('./zk-snark-client.js');
+const ZKSnarkMiner = require('./zk-snark-miner.js');
+const ZKSnarkTransaction = require('./zk-snark-transaction.js');
+const ZKSnarkCoin = require('./zk-snark-coin.js');
 
-
-async function run() {
+async function testCircuit() {
 
     // The output of each hash is a byte buffer
     let snBuf = crypto.createHash("sha256").update("random text 1").digest();
@@ -78,6 +83,23 @@ async function run() {
         console.log("Invalid proof");
     }
 
+}
+
+async function testUtils() {
+    let snBuf = crypto.createHash("sha256").update("random text 1").digest();
+    let rBuf = crypto.createHash("sha256").update("random text 2").digest();
+    let buf = Buffer.alloc(snBuf.length * 2);
+    buf.fill(snBuf, snBuf.length);
+    buf.fill(rBuf, 0, rBuf.length);
+    let correctHashBuf = crypto.createHash("sha256").update(buf).digest(); // corectHash = sha256(r||sn)
+    let badHashBuf = crypto.createHash("sha256").update("random text 3").digest();
+    let sn = zksnarkUtils.bufferToBitArray(snBuf);
+    let r = zksnarkUtils.bufferToBitArray(rBuf);
+    let correctHash = zksnarkUtils.bufferToBitArray(correctHashBuf);
+    let badHash = zksnarkUtils.bufferToBitArray(badHashBuf);
+    let input = {cm1: correctHash, cm2: badHash, sn: sn, r: r, index: 0};
+    let proof = await snarkjs.groth16.fullProve(input, "verifier.wasm", "circuit_final.zkey");
+    let publicSignals = proof.publicSignals;
 
     // Test parsePublicSignals()
     let [public_cm1, public_cm2, public_sn] = zksnarkUtils.parsePublicSignals(publicSignals);
@@ -85,9 +107,85 @@ async function run() {
     console.log(public_cm1.equals(correctHashBuf));
     console.log(public_cm2.equals(badHashBuf));
     console.log(public_sn.equals(snBuf));
-
 }
 
-run().then(() => {
+async function testSerializations() {
+    // Test block serialization/deserialization
+    let initialCoins = [];
+    initialCoins.push(ZKSnarkCoin.createNewCoin().cm);
+    initialCoins.push(ZKSnarkCoin.createNewCoin().cm);
+    let genesisBlock = ZKSnarkBlockchain.makeGenesis({
+        blockClass: ZKSnarkBlock,
+        transactionClass: ZKSnarkTransaction,
+        currencyClass: ZKSnarkCoin.ZksnarkCoin,
+        powLeadingZeroes: 12,
+        coinbaseAmount: 1
+    }, initialCoins);
+    let serializedBlock = JSON.parse(JSON.stringify(genesisBlock));
+    let deserializedBlock = ZKSnarkBlockchain.deserializeBlock(serializedBlock);
+    console.log("\nExpect true:");
+    let equals = true;
+    if (!(deserializedBlock instanceof ZKSnarkBlock)) { equals = false; }
+    if (genesisBlock.prevBlockHash !== deserializedBlock.prevBlockHash) { equals = false; }
+    if (genesisBlock.proof !== deserializedBlock.proof) { equals = false; }
+    if (genesisBlock.prevBlockHash !== deserializedBlock.prevBlockHash) { equals = false; }
+    for (let i = 0; i < genesisBlock.cmlist.length; i++) {
+      if (genesisBlock.cmlist[i].compare(deserializedBlock.cmlist[i]) !== 0) {
+        equals = false;
+      }
+    }
+    for (let i = 0; i < genesisBlock.snlist.length; i++) {
+      if (genesisBlock.snlist[i].compare(deserializedBlock.snlist[i]) !== 0) {
+        equals = false;
+      }
+    }
+    for (let i = 0; i < genesisBlock.coinbaseTransactions.length; i++) {
+      if (genesisBlock.coinbaseTransactions[i].compare(deserializedBlock.coinbaseTransactions[i]) !== 0) {
+        equals = false;
+      }
+    }
+    console.log(equals);
+
+    // Test transaction serialization/deserialization
+    let snBuf = crypto.createHash("sha256").update("random text 1").digest();
+    let rBuf = crypto.createHash("sha256").update("random text 2").digest();
+    let buf = Buffer.alloc(snBuf.length * 2);
+    buf.fill(snBuf, snBuf.length);
+    buf.fill(rBuf, 0, rBuf.length);
+    let correctHashBuf = crypto.createHash("sha256").update(buf).digest(); // corectHash = sha256(r||sn)
+    let badHashBuf = crypto.createHash("sha256").update("random text 3").digest();
+    let sn = zksnarkUtils.bufferToBitArray(snBuf);
+    let r = zksnarkUtils.bufferToBitArray(rBuf);
+    let correctHash = zksnarkUtils.bufferToBitArray(correctHashBuf);
+    let badHash = zksnarkUtils.bufferToBitArray(badHashBuf);
+    let input = {cm1: correctHash, cm2: badHash, sn: sn, r: r, index: 0};
+    let proof = await snarkjs.groth16.fullProve(input, "verifier.wasm", "circuit_final.zkey");
+    let cm = ZKSnarkCoin.createNewCoin().cm;
+    let tx = new ZKSnarkTransaction(proof, cm);
+    let serializedTx = JSON.parse(JSON.stringify(tx));
+    let deserializedTx = ZKSnarkBlockchain.deserializeTransaction(serializedTx);
+    console.log("\nExpect true:");
+    equals = true;
+    if (!(deserializedTx instanceof ZKSnarkTransaction)) { equals = false; }
+    let vKey = JSON.parse(fs.readFileSync("verification_key.json"));
+    equals = await snarkjs.groth16.verify(vKey, proof.publicSignals, proof.proof);
+    if (tx.cm.compare(deserializedTx.cm) !== 0) { equals = false; }
+    console.log(equals);
+
+
+    // Test coin serialization/deserialization
+    let coin = ZKSnarkCoin.createNewCoin();
+    let serializedCoin = JSON.parse(JSON.stringify(coin));
+    let deserializedCoin = ZKSnarkBlockchain.deserializeCoin(serializedCoin);
+    console.log("\nExpect true:");
+    equals = true;
+    if (!(deserializedCoin instanceof ZKSnarkCoin.ZksnarkCoin)) { equals = false; }
+    if (coin.cm.compare(deserializedCoin.cm) !== 0) { equals = false; }
+    if (coin.r.compare(deserializedCoin.r) !== 0) { equals = false; }
+    if (coin.sn.compare(deserializedCoin.sn) !== 0) { equals = false; }
+    console.log(equals);
+}
+
+testSerializations().then(() => {
     process.exit(0);
 });
--- zk-snark-blockchain.js
+++ zk-snark-blockchain.js
@@ -22,9 +22,78 @@ module.exports = class ZksnarkBlockchain extends Blockchain {
     // For Zksnark functionality
     block.transactions = [];
     block.coinbaseTransactions = [];
-    block.coinbaseTransactions.push(initialCoins);
+    block.cmlist = initialCoins;
+    Blockchain.cfg.currencyClass = cfg.currencyClass;
 
     return block;
+  }
+
+  /**
+   * Converts a string representation of a block to a new Block instance.
+   *
+   * @param {Object} o - An object representing a block, but not necessarily an instance of Block.
+   *
+   * @returns {ZksnarkBlock}
+   */
+  static deserializeBlock(o) {
+    if (o instanceof Blockchain.cfg.blockClass) { return o; }
+
+    let block = new Blockchain.cfg.blockClass();
+    block.chainLength = parseInt(o.chainLength, 10);
+    block.timestamp = o.timestamp;
+    block.prevBlockHash = o.prevBlockHash;
+    block.proof = o.proof;
+    block.transactions = [];
+    for (let i = 0; i < o.transactions.length; i++) {
+      block.transactions.push(Blockchain.deserializeTransaction(o.transactions[i]));
+    }
+    block.coinbaseTransactions = [];
+    for (let i = 0; i < o.coinbaseTransactions.length; i++) {
+      block.coinbaseTransactions.push(Buffer.from(o.coinbaseTransactions[i]));
+    }
+    block.snlist = [];
+    for (let i = 0; i < o.snlist.length; i++) {
+      block.snlist.push(Buffer.from(o.snlist[i]));
+    }
+    block.cmlist = [];
+    for (let i = 0; i < o.cmlist.length; i++) {
+      block.cmlist.push(Buffer.from(o.cmlist[i]));
+    }
+
+    return block;
+  }
+
+  /**
+   * Converts a string representation of a transaction to a new Transaction instance.
+   *
+   * @param {Object} o - An object representing a transaction, but not necessarily an instance of transaction.
+   *
+   * @returns {ZksnarkTransaction}
+   */
+  static deserializeTransaction(o) {
+    if (o instanceof Blockchain.cfg.transactionClass) { return o; }
+
+    let tx = new Blockchain.cfg.transactionClass();
+    tx.proof = o.proof;
+    tx.cm = Buffer.from(o.cm);
+    return tx;
+  }
+
+  /**
+   * Converts a string representation of a coin to a new Coin instance.
+   *
+   * @param {Object} o - An object representing a coin, but not necessarily an instance of coin.
+   *
+   * @returns {ZksnarkCoin}
+   */
+  static deserializeCoin(o) {
+    if (o instanceof Blockchain.cfg.currencyClass) { return o; }
+
+    let coin = new Blockchain.cfg.currencyClass()
+    coin.cm = Buffer.from(o.cm);
+    coin.r = Buffer.from(o.r);
+    coin.sn = Buffer.from(o.sn);
+    return coin;
   }
 
 }
